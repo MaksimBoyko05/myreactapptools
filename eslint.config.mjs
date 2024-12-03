f1da2ad1714a7a3c298fc78b08a9c3929c55776c
MaksimBoyko05 <vladosmakasin1@gmail.com>
2024-12-03 16:39:38 +0200

    task done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,56 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginPrettier from 'eslint-plugin-prettier';
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      prettier: pluginPrettier,
+    },
+    rules: {
+      semi: ['error', 'always'],
 
-  rules: {
-    'semi': ['error', 'always'],              
-    'indent': ['error', 4],                  
-    'no-console': 'warn',                     
-    'space-before-function-paren': ['error', 'never'],
-}
+      indent: ['error', 2],
+
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
+
+      'react/prop-types': 'warn',
+      'react/react-in-jsx-scope': 'off',
+
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          trailingComma: 'es5',
+          tabWidth: 2,
+          semi: true,
+          printWidth: 100,
+        },
+      ],
+    },
+  },
 ];
--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 
 console.log('Hello, Webpack!');
